00fcc81e8957fe462df1d0be5ae2f60300dc1c20
Dhawal Majithia <dhawalmajithia@dhawals-mac-pro.local>
2016-08-03 22:46:32 -0700

    Initial Commit

---
+++ sampleGroups/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleGroups
+//
+//  Created by Dhawal Majithia on 03/08/16.
+//  Copyright © 2016 Dhawal Majithia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
